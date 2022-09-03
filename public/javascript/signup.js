75994bc38a0e806f75ad7f6ee8e2e8c15e5280a7
remishn <nsarem65@gmail.com>
2022-09-02 19:17:13 -0500

    cleanup

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -4,9 +4,7 @@ async function signupFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    // const twitter = document.querySelector('#twitter-signup').value.trim();
-    // const github = document.querySelector('#github-signup').value.trim();
-  
+    
     if (username && email && password) {
       const response = await fetch('/api/users', {
         method: 'post',
